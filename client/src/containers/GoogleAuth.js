cece469d9084b51ca9d54b4980936b27c186ba07
rihapfirdaus <muhammadrihap448@gmail.com>
2023-12-01 02:24:41 +0700

    update

--- client/src/containers/GoogleAuth.js
+++ client/src/containers/GoogleAuth.js
@@ -5,13 +5,10 @@ import { redirect, useNavigate } from "react-router-dom";
 
 export async function action() {
   const googleProvider = new GoogleAuthProvider();
-  var authStatus;
   try {
     await signInWithRedirect(auth, googleProvider);
-
-    console.log("ini jalan ga ya");
   } catch (error) {
-    console.error(`Google Login Error [${errorCode}]: ${errorMessage}`);
+    console.error(error);
   }
   return redirect("/");
 }
