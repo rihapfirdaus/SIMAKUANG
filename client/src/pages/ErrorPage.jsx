d94e5ca98b71c139bccb7e7f91e5a4def9b66675
rihapfirdaus <muhammadrihap448@gmail.com>
2023-12-21 12:52:33 +0700

    update features

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -8,23 +8,13 @@ export default function ErrorPage() {
     <div
       id="error-page"
       style={{
-        backgroundColor: "blue",
         color: "white",
         display: "grid",
         placeItems: "center",
         height: "100vh",
       }}
     >
-      <div>
-        <span style={{ fontSize: "200px" }}>:(</span>
-        <h1 style={{ fontSize: "48px" }}>Oops!</h1>
-        <p style={{ fontSize: "20px" }}>
-          Sorry, an unexpected error has occurred.
-        </p>
-        <p>
-          <i>{error.statusText || error.message}</i>
-        </p>
-      </div>
+      <img src="error.jpg" style={{ width: "25%" }} alt="" />
     </div>
   );
 }
